679a282f52fe1b66de398e3333e1cefdf26cb916
Ga-hou <824568323@qq.com>
2019-05-05 22:26:49 +0800

    [+]新增微信组件预览页

---
+++ src/views/wx/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.scss';
+
+export default class extends React.Component {
+  render () {
+    return (
+      <div className="wx">
+        <h2>React实现微信基础组件</h2>
+        <div className="wx-preview">Preview</div>
+      </div>
+    )
+  }
+}
